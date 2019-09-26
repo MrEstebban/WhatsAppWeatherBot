31224cacfaafdff07ce2ee74cdc95f36e66629de
Esteban Pedraza <estemanps99@gmail.com>
2019-09-26 05:38:00 -0500

    Se agrega primeras funciones de forecast

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 from utils import fetch_reply
+import requests, time
 
 app = Flask(__name__)
 
@@ -14,13 +15,36 @@ def sms_reply():
     # Fetch the message
     msg = request.form.get('Body')
     tel = request.form.get('From')
-
-    respuesta = fetch_reply(msg, tel)
-    # Create reply
     resp = MessagingResponse()
-    #resp.message("You said: {}".format(msg))
-    #resp.message("Hoy esta haciendo frio :(")
-    resp.message(respuesta)
+
+    #Si el mensaje incluye la palabra "pronostico"
+    if "pronostico" in msg or "pronóstico" in msg:
+        url = 'http://api.openweathermap.org/data/2.5/forecast?q=Bogota&appid=4b912705f55a6cded8314651f6f124f5&units=metric'
+        datos_clima = requests.get(url).json()
+
+        for i in range(5):
+            hora = time.strftime('%H', time.gmtime(datos_clima['list'][i]['dt']))
+            horaInt = int(hora)
+            horaCol = " "
+
+            if horaInt-5 < 0:
+                horaCol = str((12+horaInt)-5) + ":00 pm"
+            else:
+                if horaInt-5 < 12:
+                    horaCol = str(horaInt-5) + ':00 am'
+                elif horaInt-5 == 12:
+                    horaCol = str(horaInt-5) + ':00 m'
+                else:
+                    horaCol = str((horaInt-5)-12) + ':00 pm'
+
+            resp.message("Alrededor de las {} se espera {}".format(horaCol, datos_clima['list'][i]['weather'][0]['main']))
+    else:
+        #respuesta de DialogFlow
+        respuesta = fetch_reply(msg, tel)
+
+        #Crea la respuesta al usuario
+        #resp = MessagingResponse()
+        resp.message(respuesta)
 
     return str(resp)
 
