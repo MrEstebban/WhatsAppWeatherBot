7cffefd6487ea8e8abb4cf9ac758d7b22b6a5023
Esteban Pedraza <estemanps99@gmail.com>
2019-09-30 05:11:24 -0500

    Se hace primer intento de anadir pronostico para una ciudad requerida por el usuario

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 from utils import fetch_reply
+from utils import dame_la_hora
 import requests, time
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return "Hey! My 'master' is Esteban Pedraza :D!"
 
 @app.route("/sms", methods=['POST'])
 def sms_reply():
@@ -17,42 +18,69 @@ def sms_reply():
     tel = request.form.get('From')
     resp = MessagingResponse()
 
-    #Si el mensaje incluye la palabra "pronostico"
-    if "pronostico" in msg.lower() or "pronóstico" in msg.lower():
-        url = 'http://api.openweathermap.org/data/2.5/forecast?q=Bogota&appid=4b912705f55a6cded8314651f6f124f5&units=metric&lang=es'
-        datos_clima = requests.get(url).json()
-        pronostico = ""
-        for i in range(5):
-            hora = time.strftime('%H', time.gmtime(datos_clima['list'][i]['dt']))
-            horaInt = int(hora)
-            horaCol = " "
+    mensajeRecibido = msg.lower().strip()
+    pronostico = " "
 
-            if horaInt-5 < 0:
-                horaCol = str((12+horaInt)-5) + ":00 pm"
+    #Si el ensaje incluye la palabra "pronostico"
+    if "pronostico" in mensajeRecibido or "pronóstico" in mensajeRecibido:
+        if mensajeRecibido == "pronóstico" or mensajeRecibido == "pronostico":
+            url = 'http://api.openweathermap.org/data/2.5/forecast?q=Bogota,co&appid=4b912705f55a6cded8314651f6f124f5&units=metric&lang=es'
+            datos_clima = requests.get(url).json()
+            pronostico = "El pronóstico para Bogotá es:\n"
+
+            for i in range(5):
+                hora = time.strftime('%H', time.gmtime(datos_clima['list'][i]['dt']))
+                horaInt = int(hora)
+                horaCol = dame_la_hora(horaInt)
+                desc_princ = datos_clima['list'][i]['weather'][0]['main']
+                icono = " "
+                if desc_princ == "Thunderstorm":
+                    icono = "\U000026C8"
+                elif desc_princ == "Drizzle" or desc_princ == "Rain":
+                    icono = "\U0001F327"
+                elif desc_princ == "Clear":
+                    icono = "\U00002600"
+                elif desc_princ == "Clouds":
+                    icono = "\U000026C5"
+
+                pronostico += "-A las {} se espera {} {}\n".format(horaCol, datos_clima['list'][i]['weather'][0]['description'], icono)
+                #Fin for loop
+        else:
+            cadenaInvertida = mensajeRecibido[::-1]
+            indice = 0
+            for x in cadenaInvertida:
+                if x == ' ':
+                    break
+                indice+=1
+
+            pos = (len(mensajeRecibido)-1) - indice
+            ciudad = mensajeRecibido[pos:]
+            url = 'http://api.openweathermap.org/data/2.5/forecast?q={},co&appid=4b912705f55a6cded8314651f6f124f5&units=metric&lang=es'.format(ciudad)
+            if str(requests.get(url)) == "<Response [200]>":
+                datos_clima = requests.get(url).json()
+                pronostico = "El pronóstico para {} es:\n".format(ciudad)
+
+                for i in range(5):
+                    hora = time.strftime('%H', time.gmtime(datos_clima['list'][i]['dt']))
+                    horaInt = int(hora)
+                    horaCol = dame_la_hora(horaInt)
+                    desc_princ = datos_clima['list'][i]['weather'][0]['main']
+                    icono = " "
+                    if desc_princ == "Thunderstorm":
+                        icono = "\U000026C8"
+                    elif desc_princ == "Drizzle" or desc_princ == "Rain":
+                        icono = "\U0001F327"
+                    elif desc_princ == "Clear":
+                        icono = "\U00002600"
+                    elif desc_princ == "Clouds":
+                        icono = "\U000026C5"
+
+                    pronostico += "-A las {} se espera {} {}\n".format(horaCol, datos_clima['list'][i]['weather'][0]['description'], icono)
+                    #Fin for loop
             else:
-                if horaInt-5 < 12:
-                    horaCol = str(horaInt-5) + ':00 am'
-                elif horaInt-5 == 12:
-                    horaCol = str(horaInt-5) + ':00 m'
-                else:
-                    horaCol = str((horaInt-5)-12) + ':00 pm'
-
-            desc_princ = datos_clima['list'][i]['weather'][0]['main']
-            icono = " "
-
-            if desc_princ == "Thunderstorm":
-                icono = "\U000026C8"
-            elif desc_princ == "Drizzle" or desc_princ == "Rain":
-                icono = "\U0001F327"
-            elif desc_princ == "Clear":
-                icono = "\U00002600"
-            elif desc_princ == "Clouds":
-                icono = "\U000026C5"
-
-            pronostico += "-A las {} se espera {} {}\n".format(horaCol, datos_clima['list'][i]['weather'][0]['description'], icono)
-            #Fin for loop
-
+                pronostico = "No hemos encontrado la ciudad '{}', intenta enviando el mensaje 'Pronostico nombreTuCiudad'".format(ciudad)
         resp.message(pronostico)
+        resp.message("Lo que enviaste: {}".format(mensajeRecibido))
 
     else:
         #respuesta de DialogFlow
--- utils.py
+++ utils.py
@@ -15,3 +15,14 @@ def detect_intent_from_text(text, session_id, language_code='es'):
 def fetch_reply(query, session_id):
     response = detect_intent_from_text(query, session_id)
     return response.fulfillment_text
+
+def dame_la_hora(horaInt):
+    if horaInt-5 < 0:
+        return str((12+horaInt)-5) + ":00 pm"
+    else:
+        if horaInt-5 < 12:
+            return str(horaInt-5) + ':00 am'
+        elif horaInt-5 == 12:
+            return str(horaInt-5) + ':00 m'
+        else:
+            return str((horaInt-5)-12) + ':00 pm'
